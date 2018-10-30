4ffb0d4eb13f102656e61a55140ad8296c9b3431
Chris <chrisksorensen@gmail.com>
2018-10-30 12:00:50 +0100

    AddingTeamsController, AddingTeams, ChooseYearPools

---
+++ src/sample/AddingTeamsController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+public class AddingTeamsController {
+
+    @FXML
+    void initialize() {
+
+    }
+
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AddingTeams.fxml"));
         primaryStage.setTitle("Tournament planner");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
