158b0480ed7dfbce340e7be2e3add22eaee15c86
Chris <chrisksorensen@gmail.com>
2018-10-31 11:00:42 +0100

    MatchSchedule and Tournament updated

--- src/tournament/Tournament.java
+++ src/tournament/Tournament.java
@@ -2,27 +2,25 @@ package tournament;
 
 import tournament.pool.*;
 import tournament.matchschedule.*;
+
+import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.List;
 
 public class Tournament {
+    private String name;
     private Date date;
-    private boolean active;
+    private boolean active = false;
+    private String tournamentType;
     private List<Pool> poolList;
     private MatchSchedule matchSchedule;
 
 
     // Create tournament
-    public Tournament(Date date) {
+    public Tournament(String name, Date date, String tournamentType) {
+        this.name = name;
         this.date = date;
-    }
-
-    private void editTournament() {
-
-    }
-
-    private void addTournament() {
-
+        this.tournamentType = tournamentType;
     }
 
     public boolean isActive() {
@@ -33,11 +31,15 @@ public class Tournament {
         poolList.add(new Pool(skill, year));
     }
 
-    public void createTeam(String name, int year, String skill) {
+    public void createTeam(String name, int year, String skill, String contact) {
         for (Pool pools : poolList) {
             if (pools.getSkillLevel().equals(skill) && pools.getYearGroup() == year) {
-                pools.addTeam(new Team(name, year, skill));
+                pools.addTeam(new Team(name, skill, year, contact));
             }
         }
+    }
+
+    public void createMatchSchedule(int timeBetweenMatches, List<Pool> poolList) {
+        this.matchSchedule = new MatchSchedule(timeBetweenMatches, poolList);
     }
 }
--- src/tournament/matchschedule/MatchSchedule.java
+++ src/tournament/matchschedule/MatchSchedule.java
@@ -1,12 +1,24 @@
 package tournament.matchschedule;
 
 import tournament.Match;
+import tournament.pool.Pool;
 
 import java.util.List;
 
 public class MatchSchedule {
-    int time;
-    private List<Match> matchList;
+    private int timeBetweenMatches;
+    private List<Pool> poolList;
+
+    public MatchSchedule(int timeBetweenMatches, List<Pool> poolList) {
+        this.timeBetweenMatches = timeBetweenMatches;
+        this.poolList = poolList;
+    }
+
+    private void createMatches() {
+        for (Pool pool : poolList) {
+            // do something
+        }
+    }
 
     private void updateMatchSchedule() {
 
