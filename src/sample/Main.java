cbb2917cd8542a247c11b48e47094efc18adc495
lars4909 <lbmn17@student.aau.dk>
2018-11-01 09:56:20 +0100

    Creating groups - change 2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("TournamentSetup.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("CreatingGroups.fxml"));
         primaryStage.setTitle("Tournament planner");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
