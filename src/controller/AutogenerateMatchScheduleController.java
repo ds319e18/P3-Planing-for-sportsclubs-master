4bf58002406947815eb5c486e7196f2af5e26394
Chris <chrisksorensen@gmail.com>
2018-11-26 14:20:23 +0100

    AutogenerateMatchScheduleController Updated

--- src/controller/AutogenerateMatchScheduleController.java
+++ src/controller/AutogenerateMatchScheduleController.java
@@ -13,8 +13,12 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import tournament.Match;
 import tournament.Tournament;
+import tournament.matchschedule.Field;
 import tournament.matchschedule.MatchDay;
 import tournament.pool.Pool;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AutogenerateMatchScheduleController {
     Tournament tournament;
@@ -25,20 +29,19 @@ public class AutogenerateMatchScheduleController {
     @FXML
     ComboBox comboBox;
 
-    GridPane matchScheduleGridPane;
+    GridPane matchScheduleGridPane = new GridPane();
 
     void setTournament(Tournament tournament) {
         this.tournament = tournament;
-        initilalize();
+
+        ObservableList observableList = FXCollections.observableArrayList();
+        for (int i = 0; i < tournament.getMatchSchedule().getMatchDays().size(); i++)
+            observableList.add("" + i);
+        comboBox.setItems(observableList);
     }
 
     @FXML
     void drawMatchScheduleGridPane() {
-        matchScheduleGridPane = new GridPane();
-        matchScheduleGridPane.setMaxWidth(tournament.getFieldList().size() * 300);
-        matchScheduleGridPane.setPrefWidth(tournament.getFieldList().size() * 300);
-        matchScheduleGridPane.setMinWidth(tournament.getFieldList().size() * 300);
-
         MatchDay matchDay = tournament.getMatchSchedule().getMatchDays().get(Integer.parseInt(comboBox.getValue().toString()));
         System.out.println(matchDay.toString()); //TODO FJERN
 
@@ -46,76 +49,75 @@ public class AutogenerateMatchScheduleController {
             matchScheduleGridPane.add(new Text("Bane " + (i + 1)), i, 0);
         }
 
+        int matchCounter = 1;
+        List<Integer> xdList = new ArrayList<>();
+        for (Field f : tournament.getFieldList())
+            xdList.add(1);
+
         for (Match match : matchDay.getMatches()) {
-            System.out.println(match.getFirstTeam() + " " + match.getSecondTeam() + " " + match.getDuration());//TODO FJERN
+            HBox matchHBox = createHBoxFromMatch(match, matchCounter);
 
+            matchScheduleGridPane.add(matchHBox, Integer.parseInt(match.getField().getName().substring(5)) - 1
+                    , xdList.get(Integer.parseInt(match.getField().getName().substring(5)) - 1));
 
-
-            matchScheduleGridPane.add(new Text(""), Integer.parseInt(match.getField().getName().substring(5)), 0);
+            xdList.set(Integer.parseInt(match.getField().getName().substring(5)) - 1
+            , xdList.get(Integer.parseInt(match.getField().getName().substring(5)) - 1) + 1);
+            matchCounter++;
         }
 
-
         matchScheduleScrollPane.setContent(matchScheduleGridPane);
-        matchScheduleGridPane.setVisible(true);
     }
 
-    @FXML
-    void initilalize() {
-        ObservableList observableList = FXCollections.observableArrayList();
-        System.out.println(tournament.getMatchSchedule().getMatchDays().size());
-        for (int i = 0; i < tournament.getMatchSchedule().getMatchDays().size(); i++)
-            observableList.add("" + i);
-        comboBox.setItems(observableList);
-
-
-        GridPane test = new GridPane();
+    private HBox createHBoxFromMatch(Match match, int matchCounter) {
+        HBox returnHBox = new HBox();
 
         // --- Venstre
-        /*VBox vBox1 = new VBox();
+        VBox vBox1 = new VBox();
         vBox1.setStyle("-fx-border-color: BLACK;");
 
-        Text text11 = new Text("Kamp 1");
-        Text text12 = new Text("12:00 - 12:20");
-        text11.setWrappingWidth(80);
-        text12.setWrappingWidth(80);
-        text11.setTextAlignment(TextAlignment.CENTER);
-        text12.setTextAlignment(TextAlignment.CENTER);
+        Text matchNameText = new Text("Kamp " + matchCounter);
+        Text timeIntervalText = new Text("12.20 - 12.40");
+        matchNameText.setWrappingWidth(80);
+        timeIntervalText.setWrappingWidth(80);
+        matchNameText.setTextAlignment(TextAlignment.CENTER);
+        timeIntervalText.setTextAlignment(TextAlignment.CENTER);
 
-        vBox1.getChildren().addAll(text11, text12);
+        vBox1.getChildren().addAll(matchNameText, timeIntervalText);
 
         // ---
         HBox hBox = new HBox();
 
         VBox vBox2 = new VBox();
-        //vBox2.setStyle("-fx-border-color: RED;");
+        vBox2.setStyle("-fx-border-color: RED;");
 
-        Text text21 = new Text("U9 - C");
-        Text text22 = new Text("Field 6");
-        text21.setWrappingWidth(80);
-        text22.setWrappingWidth(80);
-        text21.setTextAlignment(TextAlignment.CENTER);
-        text22.setTextAlignment(TextAlignment.CENTER);
-        vBox2.getChildren().addAll(text21, text22);
+        Text poolText = new Text("U" + match.getFirstTeam().getYearGroup() + " - " + match.getFirstTeam().getSkillLevel());
+        Text fieldText = new Text(match.getField().getName());
+        poolText.setWrappingWidth(80);
+        fieldText.setWrappingWidth(80);
+        poolText.setTextAlignment(TextAlignment.CENTER);
+        fieldText.setTextAlignment(TextAlignment.CENTER);
+        vBox2.getChildren().addAll(poolText, fieldText);
 
         VBox vBox3 = new VBox();
-        //vBox3.setStyle("-fx-border-color: BLUE;");
+        vBox3.setStyle("-fx-border-color: BLUE;");
 
-        Text text31 = new Text("Jetsmark IF 5 -");
-        Text text32 = new Text("Jetsmark IF 6");
-        text31.setWrappingWidth(107);
-        text32.setWrappingWidth(107);
-        text31.setTextAlignment(TextAlignment.CENTER);
-        text32.setTextAlignment(TextAlignment.CENTER);
-        vBox3.getChildren().addAll(text31, text32);
+        Text firstTeamText = new Text(match.getFirstTeam().getName());
+        Text secondTeamText = new Text(match.getSecondTeam().getName());
+        firstTeamText.setWrappingWidth(107);
+        secondTeamText.setWrappingWidth(107);
+        firstTeamText.setTextAlignment(TextAlignment.CENTER);
+        secondTeamText.setTextAlignment(TextAlignment.CENTER);
+        vBox3.getChildren().addAll(firstTeamText, secondTeamText);
 
         hBox.getChildren().addAll(vBox2, vBox3);
         hBox.setStyle("-fx-border-color: BLACK;");
 
-        test.add(vBox1, 0, 0);
-        test.add(hBox, 1, 0);
+        returnHBox.getChildren().add(vBox1);
+        returnHBox.getChildren().add(hBox);
 
-        matchScheduleGridPane.add(test, 0, 0);
-        matchScheduleGridPane.setMargin(hBox, new Insets(0, 0, 0, 30));*/
+        returnHBox.setMargin(hBox, new Insets(0, 30, 0, 0));
+
+        return returnHBox;
     }
 
 
