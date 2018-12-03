0ce0b38b323d32c0197a2da496793995d847492f
Chris <chrisksorensen@gmail.com>
2018-12-03 11:55:01 +0100

    Auto stash before merge of "Dev" and "origin/Dev"

--- src/tournament/matchschedule/MatchSchedule.java
+++ src/tournament/matchschedule/MatchSchedule.java
@@ -1,6 +1,7 @@
 package tournament.matchschedule;
 
 import tournament.Match;
+import tournament.pool.Pool;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -43,9 +44,9 @@ public class MatchSchedule {
 
     }
 
-    public void setNoMixedMatches(ArrayList<Match> matches) {
+    public void setNoMixedMatches(ArrayList<Pool> poolList) {
         for (MatchDay matchDay : matchDays) {
-            matchDay.setMatchesNoMix(matches);
+            matchDay.setNoMixedMatches(poolList);
         }
     }
 
